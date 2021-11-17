87003be339348423dacddda6a7c821fc5f231454
batshevazafrir <batshevaz20@gmail.com>
2021-11-17 11:23:15 +0200

    upload project

---
+++ client/art-talks/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import GetCards from './components/home';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Chat from './components/chat'
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <GetCards></GetCards>
+      </Provider> 
+      <Chat></Chat>
+      
+    </div>
+  );
+}
+
+export default App;
